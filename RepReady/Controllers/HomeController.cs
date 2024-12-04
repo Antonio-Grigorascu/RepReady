5c457df06dd7cf28b788c3d71c23b244f55f51f8
AntonioGrigorascu <grigorascu.antonio@gmail.com>
2024-12-04 22:50:29 +0200

    test2

--- RepReady/Controllers/HomeController.cs
+++ RepReady/Controllers/HomeController.cs
@@ -49,5 +49,5 @@ namespace RepReady.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-    }
+    } // test
 }
