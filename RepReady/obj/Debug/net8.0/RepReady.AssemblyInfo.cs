5bfc8ef884b8e5497cdb5ba3696f2aa620cb39fc
AntonioGrigorascu <grigorascu.antonio@gmail.com>
2024-12-08 20:57:23 +0200

    Aducere la zi

--- RepReady/obj/Debug/net8.0/RepReady.AssemblyInfo.cs
+++ RepReady/obj/Debug/net8.0/RepReady.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RepReady")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a6b08ebdda87ad6e3dc37953f5ed96d7990e832")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5826c7c1a84ec0ebb18ab944d2121ab19a6bf115")]
 [assembly: System.Reflection.AssemblyProductAttribute("RepReady")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RepReady")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
